57ec4dba3af5354a4d016261a0b262d0b5826262
LenaBultmann <lena.bultmann@gmx.de>
2024-02-07 12:46:03 +0100

    styling pokedex

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,11 @@
 import Main from "../components/Main";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="hidden">Poke-Fight</h1>
+      <img className="pkmn-logo" src="../public/pokemon_logo.png" alt="Pokemon" />
       <Main />
     </>
   );
