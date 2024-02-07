2b2ecfa50fc28d7b1ce221576cab9cc1b9c981fd
Néstor Iriondo <nestoririondo@gmail.com>
2024-02-07 23:34:14 +0100

    context and bug fixes

---
+++ src/provider/PokemonProvider.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect } from "react";
+import { SERVER } from "../constants/server";
+import axios from "axios";
+
+export const PokemonContext = createContext();
+
+const fetchUser = async () => {
+  try {
+    const response = await axios.get(`${SERVER}/user/`);
+    setUser(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const PokemonProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetchUser(setUser);
+  }, []);
+
+  const value = {
+    user,
+    setUser,
+  };
+
+  // {_id: "60e6f0e", username: "Moustafa", password: "123", pokemons: [1, 2, 3, 4, 5], battles: [{pokemon: 1, opponent: 45, result: "win"}, {pokemon: 2, opponent": 34, result: "lose"}]}
+
+  // We ask the user for user and passwod
+  // The server checks if the user exists in the database.
+  // MongoDB query: db.users.find({username: "Moustafa", password: "123"})
+  // If the user exists, it sends back the user object
+
+  return (
+    <PokemonContext.Provider value={value}>{children}</PokemonContext.Provider>
+  );
+};
