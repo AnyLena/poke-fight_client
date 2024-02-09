d4ddeb19e2312725858e45be036f717df3393d62
LenaBultmann <lena.bultmann@gmx.de>
2024-02-09 20:12:20 +0100

    adding styling to the fight view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
       <h1 className="hidden">Poke-Fight</h1>
-      <img className="pkmn-logo" src="../public/pokemon_logo.png" alt="Pokemon" />
+      <img className="pkmn-logo" src="../pokemon_logo.png" alt="Pokemon" />
       <Main />
     </>
   );
