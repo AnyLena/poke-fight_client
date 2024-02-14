ef675faa306f545f81b50c5d0c98dd96d6b1b625
LenaBultmann <lena.bultmann@gmx.de>
2024-02-14 12:42:48 +0100

    styling not-logged-in screen

--- src/views/Pokedex.jsx
+++ src/views/Pokedex.jsx
@@ -13,11 +13,10 @@ const Pokedex = () => {
       {userIsLoggedIn ? (
         <Main />
       ) : (
-        <>
+        <section class="view-not-logged">
           <h2>You want to play the game?</h2>
-          <br></br>
           <h2>You need to log in yes or yes!</h2>
-        </>
+        </section>
       )}
     </div>
   );
